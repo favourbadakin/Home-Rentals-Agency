ba84f80c7acc990133cf5f7f5fe660b0a09e65de
favourbadakin <favourbadakin@gmail.com>
2023-01-18 14:13:12 +0100

    first test deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
-import { HiOutlineSearch } from 'react-icons/hi'
+import { HiOutlineSearch } from "react-icons/hi";
 //import map from "../assets/images/maps.png";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-
 const Header = () => {
   return (
-    <header className="bg-hero-image bg-no-repeat bg-cover h-screen">
+    <header className="bg-hero-image bg-no-repeat bg-cover h-screen pb-14 sm:h-auto lg:h-screen">
       <nav className="flex space-x-72 items-end py-3 border-b border-[rgba(255,255,255,.5)] w-4/5 mx-auto md:py-4">
         <div>
           <img src={logo} alt="logo" className="w-32 h-auto md:w-full" />
@@ -36,7 +35,7 @@ const Header = () => {
         </div>
       </nav>
       {/* <div className="flex flex-col sm:flex-row items-center justify-center md:justify-between mx-32 h-full my-auto lg:overflow-x-hidden"> */}
-      <div className="grid sm:grid-cols-2 items-center place-items-center mt-12 sm:mt-0 md:mx-32 md:h-full my-auto md:gap-x-4">
+      <div className="grid sm:grid-cols-2 items-center place-items-center pt-14 mt-12 sm:mt-0 md:mx-32 md:h-full my-auto md:gap-x-4">
         <div>
           <h2 className="font-bold text-[#FFF] text-center sm:text-left w-60 text-lg mb-12 leading-6 sm:mb-0 md:text-xl lg:text-2xl xl:text-4xl lg:w-[26rem] tracking-wide lg:leading-[45px] capitalize">
             The most affordable place to stay in the san franciso bay area
@@ -49,7 +48,13 @@ const Header = () => {
               zoom={13}
               scrollWheelZoom={false}
               placeholder={<p>Loading...</p>}
-              style={{ height: "100%", width: "100%", zIndex: "10", borderRadius:'10px', marginBottom:'16px' }}
+              style={{
+                height: "100%",
+                width: "100%",
+                zIndex: "10",
+                borderRadius: "10px"
+            
+              }}
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
